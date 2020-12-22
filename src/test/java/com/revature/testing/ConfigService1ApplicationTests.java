b3ee5e96bb238dbfbbf39219926423eb032c0b78
mareo yapp <mareo1997@gmail.com>
2020-12-22 15:37:53 -0500

    Made a repo. a method and a test

---
+++ src/test/java/com/revature/testing/ConfigService1ApplicationTests.java
@@ -0,0 +1,37 @@
+package com.revature.testing;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.revature.model.Associate;
+import com.revature.repo.repo;
+
+@SpringBootTest
+class ConfigService1ApplicationTests {
+	
+	@Autowired
+	repo managerService;
+
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	void PASSfindAssociatesByManager_Id() {
+		List<Associate> a = managerService.findAssociatesByManager_Id(5);
+		assertNotEquals(0, a.size());
+	}
+	
+	@Test
+	void FAILfindAssociatesByManager_Id() {
+		List<Associate> a = managerService.findAssociatesByManager_Id(0);
+		assertEquals(0, a.size());
+	}
+
+}

637ae76f0fb0d372634abdaf8c0308a0fb231155
Ben <benjamin.johnston1998@gmail.com>
2020-12-23 12:24:18 -0500

    Began work on StagingListener, imported GSON into pom.xml

--- src/main/java/com/revature/backend/AssignmentManagerUtil/StagingListener.java
+++ src/main/java/com/revature/backend/AssignmentManagerUtil/StagingListener.java
@@ -1,5 +1,15 @@
 package com.revature.backend.AssignmentManagerUtil;
 
-public class StagingListener {
+import java.time.LocalDateTime;
+import java.util.List;
 
+import com.revature.backend.model.Batch;
+
+public interface StagingListener {
+
+	public void startListening();
+	public void checkForNewBatches();
+	public boolean triggerUpdate();
+	
+	
 }

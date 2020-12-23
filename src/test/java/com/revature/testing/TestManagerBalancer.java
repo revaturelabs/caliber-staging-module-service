ea2db2fe50be0057967096cc8f71a41e28806d5c
afkpuezo <andrewfcurry@gmail.com>
2020-12-23 16:22:13 -0500

    added @RunWith(SpringRunner.class) annotation to TestManagerBalancer

--- src/test/java/com/revature/testing/TestManagerBalancer.java
+++ src/test/java/com/revature/testing/TestManagerBalancer.java
@@ -9,7 +9,10 @@ package com.revature.testing;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 public class TestManagerBalancer {
 
     @Test

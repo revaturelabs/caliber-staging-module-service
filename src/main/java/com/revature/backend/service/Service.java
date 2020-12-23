557e616618da4d23c4ef633ad3c44e952dc74f50
mareo yapp <mareo1997@gmail.com>
2020-12-23 11:38:06 -0500

    Service

--- src/main/java/com/revature/backend/service/Service.java
+++ src/main/java/com/revature/backend/service/Service.java
@@ -1,5 +1,5 @@
 package com.revature.backend.service;
 
-public interface service {
+public interface Service {
 
 }
--- src/main/java/com/revature/backend/service/serviceimpl.java
+++
@@ -1,5 +0,0 @@
-package com.revature.backend.service;
-
-public class serviceimpl implements service {
-
-}

20f8ebcf68b02e2ff30c70352a8be69cad933bf5
mareo yapp <mareo1997@gmail.com>
2020-12-23 17:08:54 -0500

    Add notes to implementation classes

--- src/main/java/com/revature/backend/service/BackendService.java
+++ src/main/java/com/revature/backend/service/BackendService.java
@@ -6,9 +6,21 @@ import org.springframework.boot.SpringBootConfiguration;
 
 import com.revature.backend.model.Associate;
 
+/**
+ * This interface describes the methods necessary for a manager to retrieve all associates assigned to him.
+ * 
+ * @author Mareo Yapp
+ * */
 @SpringBootConfiguration()
 public interface BackendService {
 
+	/**
+	 * This method takes the managers id and returns all associates assigned to him.
+	 * 
+	 * The return value is a list of all associates assigned to the managers id number.
+	 * 
+	 * @param id : should be a valid id assigned to a manager.
+	 */
 	public List<Associate> findAssociatesByManagerId(int id);
 
 }

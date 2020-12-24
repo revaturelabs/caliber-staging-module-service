714d95939f934cfc52180e7d35c2ecc2dedc8c65
jeremycritchley <jeremycritchley@gmail.com>
2020-12-24 12:38:51 -0500

    enabled h2 console in application.properties, added CrossOrigin for localhost over AssociateController

--- src/main/java/com/revature/backend/controller/AssociateController.java
+++ src/main/java/com/revature/backend/controller/AssociateController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import com.revature.backend.model.dto.AssociateDTO;
 import com.revature.backend.service.BackendService;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 public class AssociateController {
 	
 	@Autowired

6a0466bd41ec524e8f66a18c780eb2f8c746f14f
Ben <benjamin.johnston1998@gmail.com>
2020-12-24 11:54:45 -0500

    Added test file for StagingListener, Confirmed that the StagingListener works, still working on a way to test the timer as JUNIT doesn't really like multithreaded testing.

--- src/main/java/com/revature/backend/util/StagingListenerImpl.java
+++ src/main/java/com/revature/backend/util/StagingListenerImpl.java
@@ -3,10 +3,10 @@ package com.revature.backend.util;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.Date;
@@ -19,13 +19,12 @@ import org.jboss.logging.Logger;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.revature.backend.model.Batch;
 import com.revature.backend.model.api.ApiBatchTemplate;
 
 @Component(value="StagingListener")
 public class StagingListenerImpl  implements StagingListener {
 	private LocalDateTime nextDateToWaitFor;
-	private List<ApiBatchTemplate> latestBatches = new ArrayList<>();
+	private  List<ApiBatchTemplate> latestBatchIds = new ArrayList<>();
 	public static Logger log = Logger.getLogger(StagingListenerImpl.class);
 	private DayOfWeek weeklyUpdateDay = DayOfWeek.SUNDAY;
 	@Override
@@ -34,26 +33,31 @@ public class StagingListenerImpl  implements StagingListener {
 		 * Configures timer to check for new batches on a specified day of the week every week. 
 		 * If this method does not run, the entire sorting system will fail to operate.
 		*/
+		log.info("Restarting stagingListener timer.");
 		nextDateToWaitFor = LocalDateTime.now().with(TemporalAdjusters.next(weeklyUpdateDay));
 		Date d = Date.from(nextDateToWaitFor.atZone((ZoneId.systemDefault())).toInstant());
-		new Timer().schedule(waitToCheckBatches(), d);
-		log.info("Restarting stagingListener timer.");
+		log.info(d);
+		Timer t = new Timer();
+		t.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				checkForNewBatches();
+				//t.cancel();
+			}
+		}, d);
 		
 	}
 
-	private TimerTask waitToCheckBatches() {
-		// TODO Auto-generated method stub
-		//Waits to be executed by the new Timer, then checks for any new batches
-		checkForNewBatches();
-		return null;
-	}
+	
 
 	@Override
 	public void checkForNewBatches() {
 		// TODO Auto-generated method stub
 		//Pulls ALL batches from the last year then filters to see only batches with an ending date between two specified parameters
 		log.info("Checking for new batches.....");
-		latestBatches = new ArrayList<>();
+		latestBatchIds = new ArrayList<>();
 		int year = LocalDateTime.now().getYear();
 		try {
 			URL url = new URL("https://caliber2-mock.revaturelabs.com/mock/training/batch?year="+year);
@@ -84,16 +88,17 @@ public class StagingListenerImpl  implements StagingListener {
 				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 				for( ApiBatchTemplate batch : myBatches)
 				{
-					LocalDateTime batchDate = LocalDateTime.parse(batch.getEndDate(),formatter);
+					LocalDate ld = LocalDate.parse(batch.getEndDate(),formatter);
+					LocalDateTime batchDate = LocalDateTime.of(ld,LocalDateTime.now().toLocalTime());
 					
 					if(batchDate.isAfter(lastDayChecked) && batchDate.isBefore(LocalDateTime.now()))
 					{
 						//Batch should be retrieved/id stored for retrieval from another class
-						latestBatches.add(batch);
+						latestBatchIds.add(batch);
 						log.info("New batch found, adding to latestBatches list....");
 					}
 				}
-				if(latestBatches.size() ==0)
+				if(latestBatchIds.size() ==0)
 				{
 					log.info("No new batches found.");
 				}
@@ -121,7 +126,7 @@ public class StagingListenerImpl  implements StagingListener {
 	
 	public List<ApiBatchTemplate> getLatestBatches()
 	{
-		return latestBatches;
+		return latestBatchIds;
 	}
 	
 

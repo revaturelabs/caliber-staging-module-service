d1f09433ddbdd881c865861e7b993d12710e9a2e
afkpuezo <andrewfcurry@gmail.com>
2021-01-04 11:01:13 -0500

    added skeleton of TestAssignmentController.java

---
+++ src/test/java/com/revature/backend/testing/controller/TestAssignmentController.java
@@ -0,0 +1,9 @@
+/**
+ * This class contains unit tests for the AssignmentController class
+ */
+
+package com.revature.backend.testing.controller;
+
+public class TestAssignmentController {
+    
+}

0155a064305b9daebcd215770d3fcadd40705207
Darien Sosa <45372515+Darien719@users.noreply.github.com>
2021-02-04 17:43:32 -0500

    Delete bin directory

--- bin/src/main/resources/Data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO manager VALUES  (1,'Testemail@email.email','test','user');
-INSERT INTO manager VALUES  (2,'test@revature.com','REAL_TEST_EMAIL','usery');
-INSERT INTO manager VALUES  (3,'test@email.mail','isa','cat');
-INSERT INTO batch VALUES(1,'Reston','Sophia','1','Java Full Stack');
-INSERT INTO associate VALUES (1, 'testemail@email.com', 'testA', 'associate','1A', 1, 1, 2);
-INSERT INTO associate VALUES (2, 'testemail@email.com', 'testB', 'associate','1B', 1, 1, 2);
-INSERT INTO associate VALUES (3, 'testemail@email.com', 'testC', 'associate','1C', 1, 1, 2);
-INSERT INTO associate VALUES (4, 'testemail@email.com', 'testD','associate','1D', 1, 1, 2);

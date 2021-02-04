88284e23bcd682a629b6287ed5ead931e008d737
mSheldon1986 <matthewsheldon269@gmail.com>
2021-02-04 12:45:26 -0800

    Added Test files and started on Unit tests for JwtAuthFilter

---
+++ src/test/java/com/revature/backend/testing/security/FirebaseAuthenticationManagerTest.java
@@ -0,0 +1,8 @@
+package com.revature.backend.testing.security;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FirebaseAuthenticationManagerTest {
+  
+}
---
+++ src/test/java/com/revature/backend/testing/security/JwtAuthenticationFilterTest.java
@@ -0,0 +1,42 @@
+package com.revature.backend.testing.security;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.revature.backend.security.JwtAuthenticationFilter;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationManager;
+
+@SpringBootTest
+public class JwtAuthenticationFilterTest {
+  @Mock
+  private AuthenticationManager mockAuthMan;
+
+  @InjectMocks
+  private JwtAuthenticationFilter jwtAuth;
+
+  @BeforeEach
+  public void before(){
+
+
+  }
+
+  @AfterEach
+  public void after(){
+
+  }
+
+  @Test
+  public void notNullTest(){
+    int i=7;
+    jwtAuth=new JwtAuthenticationFilter(mockAuthMan);
+    String s=null;
+    assertThat(jwtAuth).isNotNull();;
+  }
+  
+}

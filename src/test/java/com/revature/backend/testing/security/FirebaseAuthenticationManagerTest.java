da931d474f347251e004681ca7e01d9238d13358
mSheldon1986 <matthewsheldon269@gmail.com>
2021-02-05 08:34:08 -0800

    Finished testing the FirebaseAuthManager class

--- src/test/java/com/revature/backend/testing/security/FirebaseAuthenticationManagerTest.java
+++ src/test/java/com/revature/backend/testing/security/FirebaseAuthenticationManagerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import com.revature.backend.security.FirebaseAuthenticationManager;
 import com.revature.backend.security.UnauthenticatedManager;
@@ -15,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 
 @SpringBootTest
@@ -28,16 +30,20 @@ public class FirebaseAuthenticationManagerTest {
   private Authentication mockVerifiedAuth;
   private Authentication mockUnverifiedAuth;
   private Authentication mockNullCredAuth;
-  private String credentials;
+  private String verifiedCredentials;
+  private String unverifiedCredentials;
   private FirebaseToken token;
 
   @BeforeEach
   public void before()throws Exception {
     mockVerifiedAuth = new UnauthenticatedManager("username:JugemuJugemu,password:verified");
+    verifiedCredentials=(String)mockVerifiedAuth.getCredentials();
     mockUnverifiedAuth=new UnauthenticatedManager("username:Pirate,password:booty");
+    unverifiedCredentials=(String)mockUnverifiedAuth.getCredentials();
     mockNullCredAuth=new UnauthenticatedManager(null);
     token=null;
-    when(mockFireAuth.verifyIdToken(credentials)).thenReturn(token);
+    when(mockFireAuth.verifyIdToken(verifiedCredentials)).thenReturn(token);
+    when(mockFireAuth.verifyIdToken(unverifiedCredentials)).thenThrow(FirebaseAuthException.class);
   }
 
   @AfterEach
@@ -52,7 +58,22 @@ public class FirebaseAuthenticationManagerTest {
   @Test
   public void userIsVerifiedTest() throws Exception{
     Assertions.assertThrows(NullPointerException.class,()->{
-      fireAuthMan.authenticate(mockVerifiedAuth);});
+      fireAuthMan.authenticate(mockVerifiedAuth);
+    });
+  }
+
+  @Test
+  public void userIsNotVerifiedTest() throws Exception{
+    Assertions.assertThrows(BadCredentialsException.class,()->{
+      fireAuthMan.authenticate(mockUnverifiedAuth);
+    });
+  }
+
+  @Test
+  public void userIsNullTest() throws Exception{
+    Assertions.assertThrows(BadCredentialsException.class,()->{
+      fireAuthMan.authenticate(mockNullCredAuth);
+    });
   }
   
 }

e72e354d2d13f1e355afb627b214587c0e7c6022
mSheldon1986 <matthewsheldon269@gmail.com>
2021-02-08 12:52:19 -0800

    wrote more javadocs for FirebaseAuthManager

--- src/test/java/com/revature/backend/testing/security/FirebaseAuthenticationManagerTest.java
+++ src/test/java/com/revature/backend/testing/security/FirebaseAuthenticationManagerTest.java
@@ -19,11 +19,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 /**
- * This is a set of tests for the FirebaseAuthenticationManager class.It contains 4 tests related to this class<p>
- * test 1: Tests whether it is possible to instantiate an instance of the tested class<p>
- * test 2: Tests if given presumed valid credentials will return appropriate result. Issue with test:unable to create firebaseToken so throws nullpointer exception instead<p>
- * test 3: Tests if given invalid credentials the method handles appropriatly<p>
- * test 4: Tests if given null credentials that the method handles appropriately<p>
+ * This is a set of tests for the FirebaseAuthenticationManager class.It contains 4 tests related to this class.<p>
+ * test 1: Tests whether it is possible to instantiate an instance of the tested class.<p>
+ * test 2: Tests if given presumed valid credentials will return appropriate result. Issue with test:unable to create firebaseToken so throws nullpointer exception instead.<p>
+ * test 3: Tests if given invalid credentials the method handles appropriatly.<p>
+ * test 4: Tests if given null credentials that the method handles appropriately.<p>
  * @throws Exception
  * @author Matthew Sheldon
  * 
@@ -68,11 +68,19 @@ public class FirebaseAuthenticationManagerTest {
   public void after(){
 
   }
+
+  /**
+   * Tests whether FirebaseAuthenticationManager can be properly instantiated.
+   */
   @Test
   public void notNullTest(){   
     assertThat(fireAuthMan).isNotNull();
   }
-
+  /**
+   * Tests if, when given valid credentials, FirebaseAuthenticationManager will return expected result.<p>
+   * @implNote Doesn't check equality as unable to create a FirebaaseToken.
+   * @throws Exception
+   */
   @Test
   public void userIsVerifiedTest() throws Exception{
     Assertions.assertThrows(NullPointerException.class,()->{
@@ -80,13 +88,20 @@ public class FirebaseAuthenticationManagerTest {
     });
   }
 
+  /**
+   * Tests if,when given invalid credentials, it throws Exception indicating bad credentials.
+   * @throws Exception
+   */
   @Test
   public void userIsNotVerifiedTest() throws Exception{
     Assertions.assertThrows(BadCredentialsException.class,()->{
       fireAuthMan.authenticate(mockInvalidAuth);
     });
   }
-
+/**
+ * Tests if, when credentials are null, it throws Exception indicating bad credentials.
+ * @throws Exception
+ */
   @Test
   public void userIsNullTest() throws Exception{
     Assertions.assertThrows(BadCredentialsException.class,()->{

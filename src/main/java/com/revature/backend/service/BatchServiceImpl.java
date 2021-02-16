685d26a9fece3409978a8a59ca5a0d76276eed75
Jay Monari <jaymonari@outlook.com>
2021-02-16 08:10:04 -0500

    removed unneccesary interfaces in service layer

--- src/main/java/com/revature/backend/service/BatchServiceImpl.java
+++ src/main/java/com/revature/backend/service/BatchServiceImpl.java
@@ -8,27 +8,25 @@ import com.revature.backend.repository.BatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service("batchService")
-public class BatchServiceImpl implements BatchService {
+@Service
+public class BatchService {
 
 	@Autowired
     BatchRepository batchRepo;
 
-    @Override
     public List<Batch> getAllBatches() {
         return null;
     }
 
-    @Override
     public List<Batch> saveBatches(List<Batch> bList) {
         // call the appropriate Repo method to save (aka insert) batches into DB
         List<Batch> list = batchRepo.saveAll(bList);
         return list;
     }
-    
+
     public Batch getBatchById(int id) {
     	// TODO call the appropriate Repo method to save (aka insert) batches into DB
     	return batchRepo.findById(id);
     }
-    
+
 }

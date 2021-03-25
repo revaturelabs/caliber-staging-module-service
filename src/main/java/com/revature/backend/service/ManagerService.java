a8188ec48fd6fc5ccc970dbc0ead4a8522f65a49
roger moore <rogermoore06@gmail.com>
2021-03-25 10:42:19 -0500

    refactore and WebClient implementations

--- src/main/java/com/revature/backend/service/ManagerService.java
+++ src/main/java/com/revature/backend/service/ManagerService.java
@@ -37,6 +37,8 @@ public class ManagerService {
 	}
 
   public Manager getManagerByEmail(String email) {
+	  //System.out.println(email);
+	  //System.out.println(managerRepo.findAll().toString());
     return managerRepo.findByEmail(email);
   }
 

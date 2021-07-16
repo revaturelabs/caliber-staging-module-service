20d91d2dcb3dd3ccf8cabdf68ef3d5622ff8d077
WilliamSESE <87509674+WilliamSESE@users.noreply.github.com>
2021-07-16 13:54:13 +0800

    Add missing annotation
    The annotation @Repository is missing in this class. It is better to add it.

--- src/main/java/com/revature/backend/repository/ManagerRepository.java
+++ src/main/java/com/revature/backend/repository/ManagerRepository.java
@@ -7,10 +7,11 @@ import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import org.springframework.stereotype.Repository;
 import com.revature.backend.model.Manager;
 
 @Transactional
+@Repository
 public interface ManagerRepository extends JpaRepository<Manager, Integer> {
 
 	@Query(value = "SELECT * FROM manager", nativeQuery = true)
